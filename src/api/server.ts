520b6858c60d6ee283a2896ec04e6f220ee4011a
Paxton Terry <paxtonterrydev@gmail.com>
2026-05-11 12:08:25 -0600

    define base routing path in api

--- src/api/server.ts
+++ src/api/server.ts
@@ -3,8 +3,10 @@ import { ServerConfig } from "@api/config.ts";
 import { auth } from "@api/auth.ts";
 
 const app = new Hono();
-
-app.on(["POST", "GET"], "/api/auth/*", (c) => auth.handler(c.req.raw));
 app.get("/", (c) => c.text("Hello, Deno!"));
 
+const api = new Hono();
+api.on(["POST", "GET"], "/auth/*", (c) => auth.handler(c.req.raw));
+
+app.route("api", api);
 Deno.serve({ port: ServerConfig.port }, app.fetch);
